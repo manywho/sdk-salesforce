b476da688dc5ae0cd849f78b1c0c46f7b43ac946
Steve Wood <steve.wood@manywho.com>
2017-01-07 22:23:51 -0800

    Adding lightning component code

--- src/aura/ManyWhoFlow/ManyWhoFlowController.js
+++ src/aura/ManyWhoFlow/ManyWhoFlowController.js
@@ -14,31 +14,9 @@
      },
     
      afterScriptsLoaded : function(cmp, event, helper) {
-         
-         // Get the Url for the page and get out the record identifier
-         var pageUrl = window.location.href;
-         
-         // Check to see if there are any query string parameters
-         if (pageUrl.indexOf("?") > 0) {
-             pageUrl = pageUrl.substring(0, pageUrl.indexOf("?"));
-         }
-         
-         // Now assume the identifier is the last entry in the url
-         var identifier = pageUrl.substring(pageUrl.lastIndexOf("/") + 1);
-         
-         var caseAction = cmp.get("c.getStateId");
-         caseAction.setParams({ id : identifier });
-         caseAction.setCallback(this, function(response){
-             var stateId = response.getReturnValue();
-             
-             if (stateId != null &&
-                 stateId.trim().length > 0) {
-                 cmp.set("v.stateId", stateId);
-                 setTimeout(function(){ manywho.initialize(); }, 3000);
-             }
-         });
-         
-         $A.enqueueAction(caseAction);
-         
+
+        // Initialize the flow as expected
+		manywho.initialize();
+
      }
  })
