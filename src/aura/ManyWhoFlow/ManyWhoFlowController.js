2cc6d543618d0b2fbb32a1eb3ca5923debffc20e
Steve Wood <steve.wood@manywho.com>
2017-03-30 18:07:50 -0400

    Updating UI to improve lightning compatibility.

--- src/aura/ManyWhoFlow/ManyWhoFlowController.js
+++ src/aura/ManyWhoFlow/ManyWhoFlowController.js
@@ -1,31 +1,33 @@
 ({
     sendMessage : function(component, event, helper) {
-        var userAction = component.get("c.getUserInfo");
+        //var userAction = component.get("c.getUserInfo");
         
-        userAction.setCallback(this, function(response){
-            var state = response.getState();
+        //userAction.setCallback(this, function(response){
+        //    var state = response.getState();
             
-            if (state === "SUCCESS") {
-                var userInfo = response.getReturnValue();
-				var resourceUrl = $A.get('$Resource.manywho_flow');
+        //    if (state === "SUCCESS") {
+        //        var userInfo = response.getReturnValue();
+		        var resourceUrl = $A.get('$Resource.manywho_flow');
 
+        		//userInfo.sessionUrl
+        		//userInfo.sessionId
 		        var startInfo = {
-		        	tenantId: v.tenantId,
-		        	flowId: v.flowId,
-		            sessionId: userInfo.sessionId,
-		            sessionUrl: userInfo.sessionUrl,
+		        	tenantId: null,
+		        	flowId: null,
+		            sessionId: null,
+		            sessionUrl: null,
 		            resourceUrl: resourceUrl
 		        };
 		        
 		        component.find("ManyWhoFlowApp").message(startInfo);
-            }
-        });
+        //    }
+        //});
 
         $A.enqueueAction(userAction);
     },
     
     handleMessage: function(component, message, helper) {
-        var payload = message.payload;
+        /*var payload = message.payload;
         var name = payload.name;
         
         if (name === "General") {
@@ -33,6 +35,10 @@
             component.set("v.messageReceived", value);
         } else if (name === "Foo") {
             // A different response
-        }
+        }*/
+    },
+    
+    handleError: function(component, error, helper) {
+        var e = error;
     }
 })
