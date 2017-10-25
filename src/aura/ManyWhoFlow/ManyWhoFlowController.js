6930cc47104f3068dfeca390f7231c3ba041f0fb
jose <jose.collazzi@manywho.com>
2017-10-25 11:00:02 +0100

    fixing issue CORE-3208

--- src/aura/ManyWhoFlow/ManyWhoFlowController.js
+++ src/aura/ManyWhoFlow/ManyWhoFlowController.js
@@ -1,20 +1,32 @@
 ({
-    doInit: function(cmp) {
-        //cmp.set("v.mode", "DEBUG");
-        cmp.set("v.provideSessionInfo", false);
+    doInit: function(cmp, event, helper) {
         
-        var userAction = cmp.get("c.getUserInfo");
-        userAction.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                cmp.set("v.userInfo", response.getReturnValue());
-            }
-        });
+        var visualforceOriginPreview = "https://" + cmp.get("v.visualforceHostPreview");
+        var visualforceOriginProd = "https://" + cmp.get("v.visualforceHostProd");
+        
+        window.addEventListener("message", function(event) {
+            try {
+            	if (event.origin !== visualforceOriginPreview  && event.origin !== visualforceOriginProd) {
+                	return;
+            	}
                 
-        $A.enqueueAction(userAction);
-     },
+            	var sessionData = JSON.parse(event.data);
+            	cmp.set("v.sessionId", sessionData.sessionId);
+            	cmp.set("v.sessionUrl", sessionData.sessionUrl);
+            	cmp.set("v.sessionLoaded", true);
+				
+                helper.persistUrlForSecurityCheck(cmp);
+                helper.initializeFlowIfReady(cmp);
+            } catch(error) {
+                return;
+            }
+        }, false);
+    },
     
-     afterScriptsLoaded : function(cmp, event, helper) {
-         manywho.initialize();
-     }
+    afterScriptsLoaded : function(cmp, event, helper) {
+        cmp.set("v.scriptLoaded", true);
+        
+        helper.persistUrlForSecurityCheck(cmp);
+        helper.initializeFlowIfReady(cmp);
+    }
  })
--- src/aura/ManyWhoFlow/ManyWhoFlowHelper.js
+++ src/aura/ManyWhoFlow/ManyWhoFlowHelper.js
@@ -1,4 +1,57 @@
 ({
-	helperMethod : function() {
+	initializeFlowIfReady : function(cmp) {
+        console.log("trying to initialize");
+        
+        var sessionLoaded = false;
+        var scriptLoaded = false;
+        var flowValuesLoaded = false;
+        
+        if (!$A.util.isUndefined(cmp.get("v.sessionLoaded")) && cmp.get("v.sessionLoaded") ) {
+            console.log("session loaded")
+            sessionLoaded = true;
+        }
+        
+        if (!$A.util.isUndefined(cmp.get("v.scriptLoaded")) && cmp.get("v.scriptLoaded") ) {
+            console.log("script loaded")
+            scriptLoaded = true;
+        }
+        
+        if (!$A.util.isUndefined(cmp.get("v.tenantId")) && cmp.get("v.tenantId") && 
+        	!$A.util.isUndefined(cmp.get("v.flowId")) && cmp.get("v.flowId") && 
+        	!$A.util.isUndefined(cmp.get("v.flowVersionId")) && cmp.get("v.flowVersionId")) {
+            	console.log("tenantId, flowId and flowVersionId loaded")
+            	flowValuesLoaded = true;
+        }
+        
+    	if (sessionLoaded && scriptLoaded && flowValuesLoaded) {
+        	console.log("initialize success");
+       		manywho.initialize(); 
+        } else{
+        	console.log("Flow not initialized")
+        }
+    },
+    
+    persistUrlForSecurityCheck : function(cmp) {
+        if (!$A.util.isUndefined(cmp.get("v.persistUrl")) && cmp.get("v.persistUrl")) {
+        	var action = cmp.get("c.saveDomains");
+        	action.setParams({ 
+                communityName : cmp.get("v.communityName"), 
+                lightningOriginPreview: cmp.get("v.lightningOriginPreview"), 
+                lightningOriginProd: cmp.get("v.visualforceHostProd")
+            });
+        	
+            action.setCallback(this, function(response) {
+            	var state = response.getState();
+            	if (state === "ERROR") {
+                	var errors = response.getError();
+                    
+                	if (errors && errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                	}
+            	}
+        	});
+        	
+            $A.enqueueAction(action);
+        }
 	}
 })
