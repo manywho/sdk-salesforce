ecd96c4aa82c7a5a037b603a111244b9f9345bb0
jose <jose.collazzi@manywho.com>
2018-01-11 14:08:39 +0000

    fixed issue with url for managed package

--- src/aura/ManyWhoFlow/ManyWhoFlowController.js
+++ src/aura/ManyWhoFlow/ManyWhoFlowController.js
@@ -1,32 +1,38 @@
 ({
     doInit: function(cmp, event, helper) {
-        
+
         var visualforceOriginPreview = "https://" + cmp.get("v.visualforceHostPreview");
         var visualforceOriginProd = "https://" + cmp.get("v.visualforceHostProd");
-        
+        var lightningOriginPreview = "https://" + cmp.get("v.lightningOriginPreview");
+
         window.addEventListener("message", function(event) {
             try {
-            	if (event.origin !== visualforceOriginPreview  && event.origin !== visualforceOriginProd) {
-                	return;
-            	}
-                
-            	var sessionData = JSON.parse(event.data);
-            	cmp.set("v.sessionId", sessionData.sessionId);
-            	cmp.set("v.sessionUrl", sessionData.sessionUrl);
-            	cmp.set("v.sessionLoaded", true);
-				
+
+                console.log("event origin:" + event.origin);
+                if (event.origin !== visualforceOriginPreview  && event.origin !== visualforceOriginProd && event.origin !== lightningOriginPreview) {
+                    console.log("origin not acepted("+ event.origin + ") because is not *" + visualforceOriginPreview + "* or *"+ visualforceOriginProd+"* or *" + lightningOriginPreview + "*");
+
+                    return;
+                }
+
+                var sessionData = JSON.parse(event.data);
+                cmp.set("v.sessionId", sessionData.sessionId);
+                cmp.set("v.sessionUrl", sessionData.sessionUrl);
+                cmp.set("v.sessionLoaded", true);
+
                 helper.persistUrlForSecurityCheck(cmp);
+                console.log("eventlistener");
                 helper.initializeFlowIfReady(cmp);
             } catch(error) {
                 return;
             }
         }, false);
     },
-    
+
     afterScriptsLoaded : function(cmp, event, helper) {
         cmp.set("v.scriptLoaded", true);
-        
+        console.log("scriptloaded");
         helper.persistUrlForSecurityCheck(cmp);
         helper.initializeFlowIfReady(cmp);
     }
- })
+})
